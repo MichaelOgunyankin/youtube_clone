374881a03dbba26a9a1c55d6ee55effc09c42a53
MichaelOgunyankin <149902350+MichaelOgunyankin@users.noreply.github.com>
2024-03-29 16:37:21 +0000

    copuright

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -65,7 +65,7 @@ const Feed = () => {
         <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
         
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
-          Copyright © 2022 JSM Media
+          Copyright © MogunDev Media
         </Typography>
       </Box>
 
