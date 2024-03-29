3e21995f1987b9652c7005b3114e283db5ca8f3b
MichaelOgunyankin <149902350+MichaelOgunyankin@users.noreply.github.com>
2024-03-29 16:12:17 +0000

    adjusted sidebar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,4 +1,4 @@
-// import { useEffect, useState  } from 'react'
+// import React, { useEffect, useState  } from 'react'
 // import { Box, Stack, Typography } from '@mui/material'
 
 // import { Sidebar, Videos } from './'
@@ -21,8 +21,7 @@
 //     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
 //       <Box sx={{ height: { sx: 'auto', md:'92vh' }, borderRight: '1px solid #3d3d3d', px: {sx: 0, md: 2} }}>
 //         <Sidebar 
-//           selectedCategory={selectedCategory}
-//           setSelectedCategory={setSelectedCategory}
+//           selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory}
 //         />
 
 //         <Typography className='copyright' variant='body2' sx={{ mt: 1.5, color: '#fff'}}> 
@@ -42,9 +41,7 @@
 //     </Stack>
 //   )
 // }
-
 // export default Feed
-
 import React, { useEffect, useState } from "react";
 import { Box, Stack, Typography } from "@mui/material";
 
